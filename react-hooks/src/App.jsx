8deab9d1a210d83c5c96bec73e87a644b5b780d1
necro <acoustify69420@gmail.com>
2023-12-29 13:21:02 +0530

    updates

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import UseStateComponents from "./components/UseState/UseStateComponents";
 import UseReducerComponents from "./components/UseReducer/UseReducerComponents";
+import UseEffectComponents from "./components/UseEffect/UseEffectComponents";
 
 
 const App = () => {
   return (
     // <div><UseStateComponents/></div>
     <div>
-      <UseReducerComponents/>
+      <UseEffectComponents/>
     </div>
   );
 };
--- react-hooks/src/components/UseEffect/UseEffectComponents.jsx
+++ react-hooks/src/components/UseEffect/UseEffectComponents.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const UseEffectComponents = () => {
-  return (
-    <div>UseEffectComponents</div>
-  )
-}
+  const [count, setCount] = useState(0);
 
-export default UseEffectComponents
+  useEffect(() => {
+    // console.log("Use Effect is Running!");
+    // document.title = count;
+
+    const fetchData=async ()=>{
+        const api=await fetch('https://jsonplaceholder.typicode.com/todos')
+        const result=await api.json();
+        console.table(result)
+
+    }
+    fetchData(  )
+  }, []);
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+    </>
+  );
+};
+
+export default UseEffectComponents;
